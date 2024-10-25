033eb82f9cbbaf4bf572613f57f722331ff04f28
itayG98 <itai.g98@gmail.com>
2024-10-25 20:32:11 +0300

    enumaration

--- mycomp/complex.c
+++ mycomp/complex.c
@@ -1,2 +1,23 @@
 #include <stdio.h>
 #include "complex.h"
+
+Operation get_operation_type(const char *operation_str)
+{
+    if (strcmp(operation_str, "print_comp") == 0)
+        return PRINT_COMP;
+    if (strcmp(operation_str, "add_comp") == 0)
+        return ADD_COMP;
+    if (strcmp(operation_str, "sub_comp") == 0)
+        return SUB_COMP;
+    if (strcmp(operation_str, "mult_comp_real") == 0)
+        return MULT_COMP_REAL;
+    if (strcmp(operation_str, "mult_comp_img") == 0)
+        return MULT_COMP_IMG;
+    if (strcmp(operation_str, "mult_comp_comp") == 0)
+        return MULT_COMP_COMP;
+    if (strcmp(operation_str, "abs_comp") == 0)
+        return ABS_COMP;
+    if (strcmp(operation_str, "stop") == 0)
+        return STOP;
+    return INVALID_OPERATION;
+}
--- mycomp/complex.h
+++ mycomp/complex.h
@@ -1,4 +1,26 @@
 #ifndef COMPLEX_H
 #define COMPLEX_H
 
+typedef struct
+{
+    double real;
+    double imaginary;
+} Complex;
+
+typedef enum
+{
+    PRINT_COMP,
+    ADD_COMP,
+    ADD_COMP,
+    SUB_COMP,
+    MULT_COMP_REAL,
+    MULT_COMP_IMG,
+    MULT_COMP_COMP,
+    ABS_COMP,
+    STOP,
+    INVALID_OPERATION
+} Operation;
+
+Operation get_operation_type(const char *operation_str);
+
 #endif
