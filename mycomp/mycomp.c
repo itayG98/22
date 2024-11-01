51dab32e71cda5f9e7cb8c7537afabd7da4fa19d
itayG98 <itai.g98@gmail.com>
2024-11-01 17:52:11 +0200

    third way to go

--- mycomp/mycomp.c
+++ mycomp/mycomp.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 #include "mycomp.h"
 #include "complex.h"
 #include "validation.h"
@@ -23,16 +25,109 @@ static VariableTable variables[NUM_OF_VARIABLES] = {
     {'E', {0, 0}},
     {'F', {0, 0}}};
 
+static int MAX_CMD_LENGTH;
 int main()
 {
-    int flag = 1, c = 0, i = 0;
+    calculate_max_command_length();
+    char *line, *cmnd, *params;
+    int params_start_idx = -1;
     diplayRules();
-    while (1)
+    while (TRUE)
     {
-        scanf("%d", &i);
+        line = get_line();
+        if (line == NULL)
+        {
+            printf("End of input detected. Exiting...\n");
+            free(line);
+            stop();
+        }
+        if (strlen(line) != 0)
+        {
+            cmnd = extract_cmnd(line, &params_start_idx);
+            execute_command(cmnd, line, params_start_idx);
+        }
     }
-
+    free(line);
+    free(cmnd);
+    free(params);
     return 0;
+}
+
+void calculate_max_command_length(void)
+{
+    int i;
+    for (i = 0; i < NUM_OF_CMNDS; i++)
+    {
+        int len = strlen(commandTable[i].command);
+        if (len > MAX_CMD_LENGTH)
+        {
+            MAX_CMD_LENGTH = len;
+        }
+    }
+}
+
+char *extract_cmnd(const char *line, int *params_start_idx)
+{
+    char *cmnd = malloc((MAX_CMD_LENGTH + 1) * sizeof(char));
+    int i = 0;
+    while (line[i] && i < 15 && (islower(line[i]) || line[i] == '_'))
+    {
+        cmnd[i] = line[i];
+        i++;
+    }
+    cmnd[i] = '\0';
+    *params_start_idx = i;
+    return cmnd;
+}
+
+void execute_command(char *cmnd, char *line, int params_start_idx)
+{
+    int i = 0;
+    for (i = 0; i < NUM_OF_CMNDS; i++)
+    {
+        if (strcmp(cmnd, commandTable[i].command) == 0)
+        {
+            if (commandTable[i].action)
+            {
+                printf("Executing : %s\n", commandTable[i].command);
+                printf("using : %s\n", line + params_start_idx);
+                /*commandTable[i].action(); */
+            }
+            return;
+        }
+    }
+    printf("Unknown command: %s\n", cmnd);
+}
+
+char *get_line()
+{
+    char *line = malloc(sizeof(char) * MAX_LINE_LENGTH);
+    if (!line)
+    {
+        perror("Unable to allocate memory for line");
+        return NULL;
+    }
+    if (fgets(line, sizeof(char) * MAX_LINE_LENGTH, stdin) == NULL)
+    {
+        return NULL;
+    }
+    line[strcspn(line, "\n")] = 0;
+    remove_spaces_and_tabs(line);
+    return line;
+}
+
+void remove_spaces_and_tabs(char *str)
+{
+    int i = 0, j = 0;
+    while (str[i])
+    {
+        if (str[i] != ' ' && str[i] != '\t')
+        {
+            str[j++] = str[i];
+        }
+        i++;
+    }
+    str[j] = '\0';
 }
 
 void stop(void)
@@ -43,8 +138,7 @@ void stop(void)
 void diplayRules(void)
 {
     /* Introduction and instructions for user */
-    printf("Welcome to COMPLEX OPERATIONS.\n");
-    printf("Let's explore complex number operations together.\n");
+    printf("Welcome to the complex numbers calculator.\n");
     printf("First consider this rules and examples :\n");
     printf("1. Valid complex variable names: A, B, C, D, E, F.\n");
     printf("2. Valid operations are as follows:\n");
--- mycomp/mycomp.h
+++ mycomp/mycomp.h
@@ -4,10 +4,16 @@
 #include "complex.h"
 #include "validation.h"
 
+#define MAX_LINE_LENGTH 256
 #define NUM_OF_CMNDS 9
 #define NUM_OF_VARIABLES 6
 
-void displayRules(void);
+char *get_line(void);
+void calculate_max_command_length(void);
+char *extract_cmnd(const char *line, int *params_start_idx);
+void remove_spaces_and_tabs(char *str);
+void execute_command(char *cmnd, char *params, int params_start_idx);
+void display_rules(void);
 void stop(void);
 
 typedef struct
