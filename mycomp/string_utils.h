b3ef33b0a6adbbc96d92fa029938751b68d1d95d
itayG98 <itai.g98@gmail.com>
2024-12-07 18:35:31 +0200

    Added comemnts to string utils

--- mycomp/string_utils.h
+++ mycomp/string_utils.h
@@ -3,19 +3,29 @@
 
 #include "common_types.h"
 
+/* MACRO to skip str pointer to the firsst non whitespace*/
 #define SKIP_SPACES(str)                                                 \
     while ((*str == ' ' || *str == '\t') && *str != '\0' && *str != EOF) \
         str++;
 
+/* MACRO to skip str pointer to the first non whitespace and increasing i parameter*/
 #define SKIP_SPACES_WITH_i(str)                                                                \
     while (((str)[i] && ((str)[i] == ' ' || (str)[i] == '\t') && *str != '\0' && *str != EOF)) \
         (i)++;
 
-char *copyStr(const char *source, const int line_length);
+/*Helper method to copy a string into new allocated memory*/
+char *copyStr(const char *source, const int length);
+/*Helper method to return a charecter if it is the only non-white charecter in a string*/
 char getOnlyChar(const char *str);
+/*Helper method to wether a charecter is white charecter*/
 BOOLEAN isTabOrSpace(char c);
+/*Validation method before a string inserted into atof method*/
 BOOLEAN isValidNumString(const char *str);
+/*Helper method check wether a given string contains only white charecters*/
 BOOLEAN isSpacesString(const char *str);
-BOOLEAN endsWithDelimiter(char *str, char delimiter);
+/*
+ * Helper method to tokenize a string.
+ * Unlike `strtok()`, consecutive delimiters return an empty string instead of skipping tokens.
+ */
 char *my_strsep(char **stringp, const char *delim);
 #endif
